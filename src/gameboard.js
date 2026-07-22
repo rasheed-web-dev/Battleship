00e806be7aa789a0b378849722cbe61a3a54eb3b
rasheed-web-dev <274648074+rasheed-web-dev@users.noreply.github.com>
2026-07-22 22:41:50 +0300

    feat: write place function

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,8 +2,37 @@ class Gameboard {
   constructor(size = 7) {
     this.size = size;
     this.board = Array.from({ length: this.size }, () =>
-      Array(this.size).fill(null)
+      Array(this.size).fill('e')
     );
+  }
+
+  place(ship, y, x, horizontal = true) {
+    let shipLen = ship.len;
+
+    let validPosition;
+    if (horizontal) {
+      validPosition = shipLen + x <= this.size ? true : false;
+    } else {
+      validPosition = shipLen + y <= this.size ? true : false;
+    }
+
+    if (!validPosition) {
+      throw new Error('Invalid Position');
+    }
+
+    if (horizontal) {
+      let placement = x;
+      for (let i = 0; i < shipLen; i++) {
+        this.board[y][placement] = 's';
+        placement++;
+      }
+    } else {
+      let placement = y;
+      for (let i = 0; i < shipLen; i++) {
+        this.board[placement][x] = 's';
+        placement++;
+      }
+    }
   }
 }
 
