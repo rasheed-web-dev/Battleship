bf16243c723399ef28f2a55a63d11bfcf3635096
rasheed-web-dev <274648074+rasheed-web-dev@users.noreply.github.com>
2026-08-02 00:37:06 +0300

    feat: render the game

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import './styles.css';
+import { createGame } from './game.js';
 
-console.log('success');
+const game = createGame();
+
+const statusDisplay = document.getElementById('status-display');
+const playerBoardElement = document.getElementById('player-board');
+const cpuBoardElement = document.getElementById('cpu-board');
+
+function renderBoards() {
+  playerBoardElement.innerHTML = '';
+  cpuBoardElement.innerHTML = '';
+
+  const state = game.getState();
+
+  state.player1Board.forEach((row) => {
+    row.forEach((cellValue) => {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+
+      if (cellValue === 's') cell.style.backgroundColor = '#639a1f';
+
+      playerBoardElement.appendChild(cell);
+    });
+  });
+
+  state.player2Board.forEach((row, y) => {
+    row.forEach((cellValue, x) => {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+
+      // data attributes so I know what cell is pressed
+      cell.dataset.y = y;
+      cell.dataset.x = x;
+
+      cell.addEventListener('click', handleAttack);
+
+      cpuBoardElement.appendChild(cell);
+    });
+  });
+
+  updateStatus(state);
+}
+
+function handleAttack(e) {
+  const y = parseInt(e.target.dataset.y);
+  const x = parseInt(e.target.dataset.x);
+
+  game.playRound(y, x);
+
+  renderBoards();
+}
+
+function updateStatus(state) {
+  if (state.isGameOver) {
+    statusDisplay.textContent = `${state.currentTurn === 'human' ? 'CPU' : 'You'} Won!`;
+  } else {
+    statusDisplay.textContent =
+      state.currentTurn === 'human' ? 'Your Turn' : 'Enemy Turn';
+  }
+}
+
+renderBoards();
